c122d3dc7bad2c2d04d9da924856338938c2c7fc
Praven18 <elormk18@gmail.com>
2020-12-30 14:05:50 -0500

    file

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+    class Program
+    { 
+        public class randomNumber {
+            private readonly Random _random = new Random();
+
+            public int GetranNum()
+            {
+                return _random.Next(1, 7);
+            }
+        }
+        static void Main()
+        {
+            var gen = new randomNumber();
+            string num = "";
+            for (int i = 0; i < 4; i++)
+                num = num + gen.GetranNum().ToString();
+            int count = 0;          
+            string val;
+            bool check = true;
+            Console.WriteLine("Your input: " + num);
+            while (count < 10) {
+                Console.WriteLine("Enter int: ");
+                val = Console.ReadLine();
+                check = true;
+                for(int i = 0; i < 4; i++)
+                {
+                    if (String.Equals(num[i],val[i]))
+                        Console.WriteLine("+ " + val[i]);
+                    else if(num.Contains(val[i])) {
+                        Console.WriteLine("- " + val[i]);
+                        check = false;
+                    }
+                    else
+                        check = false;
+                }
+                if(check) {
+                    count = 11111;
+                    Console.WriteLine("You win");
+                }
+                else if (count >= 10)
+                    Console.WriteLine("You Lose");
+
+            }         
+        }
+    }
